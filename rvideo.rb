6241ee53d26f613c8f2563f3098584a9f3326e8e
Delyan Angelov <delian66@gmail.com>
2009-01-23 00:38:51 +0000

    Fix the rvideo installation task - it previously failed, complaining with ' sudo: cd: command not found '

--- rvideo.rb
+++ rvideo.rb
@@ -1,10 +1,10 @@
 namespace :panda do
   
   desc "Install RVideo"
-  task :rvideo do
+  task :install_rvideo do
      ["rm -rf rvideo",
        "git clone git://github.com/jaikoo/rvideo.git",
-       "cd rvideo && rake install_gem && cp lib/rvideo/tools/*.rb  /usr/lib/ruby/gems/1.8/gems/rvideo-0.9.4/lib/rvideo/tools/"].each {|cmd| sudo cmd}
+       "cd rvideo && sudo rake install_gem && sudo cp lib/rvideo/tools/*.rb  /usr/lib/ruby/gems/1.8/gems/rvideo-0.9.4/lib/rvideo/tools/"].each {|cmd| run cmd}
   end
   
 end
